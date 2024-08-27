835684b710fe11ecd0ee1db7a14d4e5ea8db8b2f
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-27 12:24:34 +0530

    Making a hard Coded Cart without any Functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,13 @@
 import React from "react";
 import cartIcon from "../../assets/cartIcon.svg";
 import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
+        <CartIcon />
         <img style={{ width: 25 }} src={cartIcon} alt="Cart" />
       </span>
       <span>Your Cart</span>
